9930d258fdee563d0b26d23daa81db08330be1a3
ccoverstreet <coverst2@vols.utk.edu>
2023-07-25 10:27:07 -0400

    Added status code checking to util.js

--- frontend/src/lib/util.js
+++ frontend/src/lib/util.js
@@ -18,4 +18,12 @@ export function JSONPostRequest(url, body) {
 		},
 		body: JSON.stringify(body)
 	})
+		.then(async res => {
+			if (res.status < 200 || res.status >= 400) {
+				const content = await res.json();
+				throw new Error(res, { cause: content } );
+			}
+
+			return res.json();
+		})
 }
