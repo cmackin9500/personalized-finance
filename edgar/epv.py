4d166920d392a22819653045af37916a146c305b
cmackin9500 <casey@mackinnon.org>
2024-03-23 17:01:19 -0600

    Add formatting for epv data

--- edgar/epv.py
+++ edgar/epv.py
@@ -10,10 +10,15 @@ depreciation_adjustment = ["Premises and Equipment", "Current Year Revenue", "Pr
                            "Depreciation and Amortization", "CAPEX", "Growth CAPEX", "Zero-growth CAPEX", "Depreciation Adjustment"]
 
 titleFont = Font(name='Arial',size=10, bold=True, italic=False, vertAlign=None, underline='none', strike=False, color='000000')
+textFont  = Font(name='Arial',size=10, bold=False, italic=False, vertAlign=None, underline='none', strike=False, color='000000')
+
 greyFill = PatternFill(fill_type="solid", start_color='999997', end_color='999997')
 
 epvFill = PatternFill(fill_type="solid", start_color="c6e6c1", end_color="c6e6c1")
 greenFill = PatternFill(fill_type="solid", start_color='7eab76', end_color='7eab76')
+
+depAdjFill = PatternFill(fill_type="solid", start_color="fef2cc", end_color="fef2cc")
+depAdjDataFill = PatternFill(fill_type="solid", start_color="cfe2f3", end_color="cfe2f3")
 
 titleFill = PatternFill(fill_type="solid", start_color='ebebeb', end_color='ebebeb')
 thinBorder = Side(style="thin", color="000000")
@@ -40,6 +45,24 @@ def EPV_titles(wb_EPV, row, col):
         else:
             cell.border = Border(left=thickBorder, top=noBorder, right=noBorder, bottom=noBorder)
         row += 1
+    
+def dep_adj_titles(wb_EPV, row, col):
+    for i, value in enumerate(depreciation_adjustment):
+        wb_EPV.cell(row=row, column=col, value=value)
+        cell = wb_EPV[f"B{row}"]
+        cell.font = textFont
+        cell.fill = depAdjFill
+        
+        if (row == 24):
+            cell.border = Border(left=thickBorder, top=thickBorder, right=noBorder, bottom=noBorder)
+        elif (row == 31):
+            cell.border = Border(left=thickBorder, top=noBorder, right=noBorder, bottom=thinBorder)
+        elif (row == 32):
+            cell.font = titleFont
+            cell.border = Border(left=thickBorder, top=noBorder, right=noBorder, bottom=thickBorder)
+        else:
+            cell.border = Border(left=thickBorder, top=noBorder, right=noBorder, bottom=noBorder)
+        row += 1
 
 def fill_EPV_data(wb_EPV, row, col):
     wb_EPV.column_dimensions['C'].width = 25
@@ -62,6 +85,24 @@ def fill_EPV_data(wb_EPV, row, col):
             cell.border = Border(left=thinBorder, top=noBorder, right=noBorder, bottom=noBorder)
         row += 1 
 
+def fill_dep_adj_data(wb_EPV, row, col):
+    for i, value in enumerate(depreciation_adjustment):
+        wb_EPV.cell(row=row, column=col, value="")
+        cell = wb_EPV[f"C{row}"]
+        cell.font = textFont
+        cell.fill = depAdjDataFill
+        
+        if (row == 24):
+            cell.border = Border(left=thinBorder, top=thickBorder, right=thinBorder, bottom=noBorder)
+        elif (row == 31):
+            cell.border = Border(left=thinBorder, top=noBorder, right=thinBorder, bottom=thinBorder)
+        elif (row == 32):
+            cell.font = titleFont
+            cell.border = Border(left=thinBorder, top=noBorder, right=thinBorder, bottom=thickBorder)
+        else:
+            cell.border = Border(left=thinBorder, top=noBorder, right=thinBorder, bottom=noBorder)
+        row += 1
+
 ticker = "DOMI"
 path = f"./EPV/{ticker}.xlsx"	
 
@@ -71,6 +112,10 @@ wb.create_sheet("EPV")
 row, col = 3, 2
 wb_EPV = wb["EPV"]
 EPV_titles(wb_EPV, row, col)
+row = 24
+dep_adj_titles(wb_EPV, row, col)
 row, col = 3, 3
 fill_EPV_data(wb_EPV, row, col)
+row = 24
+fill_dep_adj_data(wb_EPV, row, col)
 wb.save(path)
